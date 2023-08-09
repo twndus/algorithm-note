d389cc3817757a4cf5e968f4768bba0a087d85aa
리주연 <leejuyeon@lijuyeons-MacBook-Pro.local>
2023-08-09 23:21:19 +0900

    feat: linear search

---
+++ algorithm/search/linear-search.py
@@ -0,0 +1,28 @@
+# linear search
+
+def linear_search(array, elem):
+    index = -1
+    for i, e in enumerate(array):
+        if elem == e:
+            index = i
+    return index
+
+if __name__ == '__main__':
+    array = []
+
+    # input array size
+    array_size = int(input("배열의 크기를 입력하세요: "))
+
+    for i in range(array_size):
+        array_elem = input(f"[{i}]번째 요소를 입력하세요: ")
+        array.append(array_elem)
+
+    elem = input("검색할 요소를 입력하세요: ")
+
+    index = linear_search(array, elem)
+    
+    if index == -1:
+        print(f"요소 {elem}가 배열{array_elem} 에 없습니다.")
+    else:
+        print(f"요소 {elem}는 {index}에 있습니다.")
+
